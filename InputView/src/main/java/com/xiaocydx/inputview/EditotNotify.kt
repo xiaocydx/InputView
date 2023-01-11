9bea7493b80db33361607656748c79eca9fa56a4
xcc <xiaocydx@163.com>
2023-01-11 18:27:50 +0800

    perf: 简化和补充示例代码

--- InputView/src/main/java/com/xiaocydx/inputview/EditotNotify.kt
+++ InputView/src/main/java/com/xiaocydx/inputview/EditotNotify.kt
@@ -4,32 +4,13 @@ package com.xiaocydx.inputview
  * 当前显示的[Editor]，若为`null`，则当前是初始化状态
  */
 @Suppress("UNCHECKED_CAST")
-val <T : Editor> EditorAdapter<T>.currentEditor: T?
+val <T : Editor> EditorAdapter<T>.current: T?
     get() = editorView?.current as? T
 
 /**
- * 当前是否显示IME
+ * 当前显示的[Editor]是否为[editor]
  */
-val EditorAdapter<*>.isShowIme: Boolean
-    get() = editorView != null
-            && editorView!!.current != null
-            && editorView!!.current === editorView!!.ime
-
-/**
- * 通知显示IME，若未对[InputView]设置当前[EditorAdapter]，则调用无效
- */
-fun EditorAdapter<*>.notifyShowIme() {
-    val editorView = editorView ?: return
-    editorView.ime?.let(editorView::showChecked)
-}
-
-/**
- * 通知隐藏IME，若未对[InputView]设置当前[EditorAdapter]，则调用无效
- */
-fun EditorAdapter<*>.notifyHideIme() {
-    val editorView = editorView ?: return
-    editorView.ime?.let(editorView::hideChecked)
-}
+fun <T : Editor> EditorAdapter<T>.isShowing(editor: T): Boolean = current === editor
 
 /**
  * 通知显示[editor]，若未对[InputView]设置当前[EditorAdapter]，则调用无效
@@ -49,8 +30,21 @@ fun <T : Editor> EditorAdapter<T>.notifyHide(editor: T) {
  * 通知隐藏当前[Editor]，若未对[InputView]设置当前[EditorAdapter]，则调用无效
  */
 fun <T : Editor> EditorAdapter<T>.notifyHideCurrent() {
-    val editorView = editorView ?: return
-    editorView.current?.let(editorView::hideChecked)
+    current?.let(::notifyHide)
+}
+
+/**
+ * 通知显示IME，若未对[InputView]设置当前[EditorAdapter]，则调用无效
+ */
+fun EditorAdapter<*>.notifyShowIme() {
+    editorView?.apply { ime?.let(::showChecked) }
+}
+
+/**
+ * 通知隐藏IME，若未对[InputView]设置当前[EditorAdapter]，则调用无效
+ */
+fun EditorAdapter<*>.notifyHideIme() {
+    editorView?.apply { ime?.let(::hideChecked) }
 }
 
 /**
@@ -60,10 +54,5 @@ fun <T : Editor> EditorAdapter<T>.notifyHideCurrent() {
  * 2. 当前已显示[editor]，则显示IME。
  */
 fun <T : Editor> EditorAdapter<T>.notifyToggle(editor: T) {
-    val editorView = editorView ?: return
-    if (editorView.current != editor) {
-        editorView.showChecked(editor)
-    } else {
-        notifyShowIme()
-    }
+    if (current !== editor) notifyShow(editor) else notifyShowIme()
 }
--- app/src/main/java/com/xiaocydx/inputview/MessageListActivity.kt
+++ app/src/main/java/com/xiaocydx/inputview/MessageListActivity.kt
@@ -1,12 +1,11 @@
 package com.xiaocydx.inputview
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.MotionEvent
 import android.view.MotionEvent.ACTION_DOWN
-import android.view.View
-import android.widget.EditText
+import android.view.Window
 import android.widget.ImageView
-import android.widget.TextView
 import androidx.annotation.DrawableRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
@@ -39,8 +38,8 @@ class MessageListActivity : AppCompatActivity() {
             editText = etMessage
             editorMode = EditorMode.ADJUST_PAN
             this.editorAdapter = editorAdapter
-            // 不运行动画
-            // setEditorAnimator(null)
+            // 不运行Editor的过渡动画
+            // editorAnimator = NopEditorAnimator()
         }
 
         // 3. 初始化RecyclerView的属性
@@ -52,95 +51,77 @@ class MessageListActivity : AppCompatActivity() {
             scrollToPosition(0)
         }
 
-        // 4. 关联各场景的交互
-        RecyclerViewTouchHideEditor(editorAdapter).attach(rvMessage)
-        RecyclerViewScrollToFirst(rvMessage).attach(etMessage, editorAdapter)
-        EditorToggleController(tvVoice, ivVoice, ivEmoji, ivExtra, etMessage, editorAdapter).attach()
+        // 4. 处理输入场景的交互
+        handleScroll()
+        handleTouch()
+        handleToggle(editorAdapter)
     }
 
     /**
-     * 触摸RecyclerView隐藏当前[MessageEditor]
+     * 1. `etMessage`的高度更改，`rvMessage`滚动到首位。
+     * 2. 显示的[MessageEditor]更改，`rvMessage`滚动到首位。
      */
-    private class RecyclerViewTouchHideEditor(
-        private val editorAdapter: MessageEditorAdapter
-    ) : RecyclerView.OnItemTouchListener {
-
-        fun attach(rv: RecyclerView) {
-            rv.addOnItemTouchListener(this)
+    private fun ActivityMessageListBinding.handleScroll() {
+        val scrollToFirstIfNecessary = fun() {
+            val lm = rvMessage.layoutManager as? LinearLayoutManager ?: return
+            if (lm.findFirstCompletelyVisibleItemPosition() == 0) return
+            rvMessage.scrollToPosition(0)
         }
 
-        override fun onInterceptTouchEvent(rv: RecyclerView, e: MotionEvent): Boolean {
-            if (e.action == ACTION_DOWN && editorAdapter.currentEditor !== VOICE) {
-                editorAdapter.notifyHideCurrent()
-            }
-            return false
-        }
-
-        override fun onTouchEvent(rv: RecyclerView, e: MotionEvent) = Unit
-
-        override fun onRequestDisallowInterceptTouchEvent(disallowIntercept: Boolean) = Unit
-    }
-
-    /**
-     * `etMessage`的高度更改或者显示的[MessageEditor]更改，RecyclerView滚动到首位（反转布局）
-     */
-    private class RecyclerViewScrollToFirst(
-        private val rvMessage: RecyclerView
-    ) : View.OnLayoutChangeListener, EditorVisibleListener<Editor> {
-
-        fun attach(etMessage: EditText, editorAdapter: MessageEditorAdapter) {
-            etMessage.addOnLayoutChangeListener(this)
-            editorAdapter.addEditorVisibleListener(this)
-        }
-
-        override fun onLayoutChange(
-            v: View,
-            left: Int, top: Int, right: Int, bottom: Int,
-            oldLeft: Int, oldTop: Int, oldRight: Int, oldBottom: Int
-        ) {
-            if (right - left == 0) return
+        etMessage.addOnLayoutChangeListener listener@{ _, left, top, right, bottom,
+                                                       _, oldTop, _, oldBottom ->
+            if (right - left == 0) return@listener
             val oldHeight = oldBottom - oldTop
             val newHeight = bottom - top
             if (oldHeight != newHeight) scrollToFirstIfNecessary()
         }
 
-        override fun onVisibleChanged(previous: Editor?, current: Editor?) {
-            scrollToFirstIfNecessary()
-        }
-
-        private fun scrollToFirstIfNecessary() {
-            val lm = rvMessage.layoutManager as? LinearLayoutManager ?: return
-            if (lm.findFirstCompletelyVisibleItemPosition() == 0) return
-            rvMessage.scrollToPosition(0)
-        }
+        inputView.editorAdapter.addEditorVisibleListener { _, _ -> scrollToFirstIfNecessary() }
     }
 
     /**
-     * [MessageEditor]的视图和图标切换控制器
+     * 1. 在动画运行中决定是否拦截触摸事件。
+     * 2. 触摸RecyclerView隐藏当前[MessageEditor]。
      */
-    private class EditorToggleController(
-        private val tvVoice: TextView,
-        private val ivVoice: ImageView,
-        private val ivEmoji: ImageView,
-        private val ivExtra: ImageView,
-        private val etMessage: EditText,
-        private val editorAdapter: MessageEditorAdapter
-    ) : EditorVisibleListener<MessageEditor> {
-        private val actions = mutableMapOf<MessageEditor, Action>()
-
-        fun attach() {
-            actions[VOICE] = Action(ivVoice, R.mipmap.ic_message_editor_voice)
-            actions[EMOJI] = Action(ivEmoji, R.mipmap.ic_message_editor_emoji)
-            actions[EXTRA] = Action(ivExtra, R.mipmap.ic_message_editor_extra, isKeep = true)
-            // 初始化各个按钮显示的图标
-            actions.forEach { it.value.showSelfIcon() }
-            ivVoice.onClick { editorAdapter.notifyToggle(VOICE) }
-            ivEmoji.onClick { editorAdapter.notifyToggle(EMOJI) }
-            ivExtra.onClick { editorAdapter.notifyToggle(EXTRA) }
-            editorAdapter.addEditorVisibleListener(this)
+    @SuppressLint("ClickableViewAccessibility")
+    private fun ActivityMessageListBinding.handleTouch() {
+        val delegate = window.callback
+        window.callback = object : Window.Callback by delegate {
+            override fun dispatchTouchEvent(e: MotionEvent): Boolean {
+                // 实际场景可能需要在动画运行中拦截触摸事件，避免造成较差的交互体验
+                // if (e.action == ACTION_DOWN && inputView.editorAnimator.isRunning) return false
+                return delegate.dispatchTouchEvent(e)
+            }
         }
 
-        override fun onVisibleChanged(previous: MessageEditor?, current: MessageEditor?) {
+        rvMessage.addOnItemTouchListener(object : RecyclerView.OnItemTouchListener {
+            override fun onInterceptTouchEvent(rv: RecyclerView, e: MotionEvent): Boolean {
+                if (e.action == ACTION_DOWN && inputView.editorAdapter.current !== VOICE) {
+                    inputView.editorAdapter.notifyHideCurrent()
+                }
+                return false
+            }
+
+            override fun onTouchEvent(rv: RecyclerView, e: MotionEvent) = Unit
+
+            override fun onRequestDisallowInterceptTouchEvent(disallowIntercept: Boolean) = Unit
+        })
+    }
+
+    /**
+     * 处理[MessageEditor]的视图和图标切换
+     */
+    private fun ActivityMessageListBinding.handleToggle(editorAdapter: MessageEditorAdapter) {
+        val actions = mutableMapOf<MessageEditor, Action>()
+        actions[VOICE] = Action(ivVoice, R.mipmap.ic_message_editor_voice)
+        actions[EMOJI] = Action(ivEmoji, R.mipmap.ic_message_editor_emoji)
+        actions[EXTRA] = Action(ivExtra, R.mipmap.ic_message_editor_extra, isKeep = true)
+        // 初始化各个按钮显示的图标
+        actions.forEach { it.value.showSelfIcon() }
+        ivVoice.onClick { editorAdapter.notifyToggle(VOICE) }
+        ivEmoji.onClick { editorAdapter.notifyToggle(EMOJI) }
+        ivExtra.onClick { editorAdapter.notifyToggle(EXTRA) }
+        editorAdapter.addEditorVisibleListener { previous, current ->
             if (previous === VOICE) {
                 tvVoice.isVisible = false
                 etMessage.isVisible = true
@@ -152,24 +133,24 @@ class MessageListActivity : AppCompatActivity() {
             actions.forEach { it.value.showSelfIcon() }
             current?.let(actions::get)?.takeIf { !it.isKeep }?.showImeIcon()
         }
+    }
 
-        private class Action(
-            private val view: ImageView,
-            @DrawableRes val iconResId: Int,
-            @DrawableRes val imeResId: Int = R.mipmap.ic_message_editor_ime,
-            val isKeep: Boolean = false
-        ) {
-            private var currentResId = 0
+    private class Action(
+        private val view: ImageView,
+        @DrawableRes val iconResId: Int,
+        @DrawableRes val imeResId: Int = R.mipmap.ic_message_editor_ime,
+        val isKeep: Boolean = false
+    ) {
+        private var currentResId = 0
 
-            fun showSelfIcon() = showIcon(iconResId)
+        fun showSelfIcon() = showIcon(iconResId)
 
-            fun showImeIcon() = showIcon(imeResId)
+        fun showImeIcon() = showIcon(imeResId)
 
-            private fun showIcon(resId: Int) {
-                if (currentResId == resId) return
-                currentResId = resId
-                view.setImageResource(resId)
-            }
+        private fun showIcon(resId: Int) {
+            if (currentResId == resId) return
+            currentResId = resId
+            view.setImageResource(resId)
         }
     }
 }
